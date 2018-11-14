620f21840a15d836dc42458f39d6f6d566ed7414
maikelb <maikel.bolderdijk@student.hu.nl>
2018-11-14 11:30:46 +0100

    Change check ready for testing

--- device_change_check.lua
+++ device_change_check.lua
@@ -25,12 +25,27 @@ end
 
 function __LINE__() return debug.getinfo(2, 'l').currentline end
 
---  [[  Sends multiple items directly to an API ]]  --
+--  [[ Compares 2 tables(t1, t2) and returns the differences ]]  --
+--  [[ @return table with differences ]]  --
+function GetTableDiff(t1,t2)
+  if type(t1) ~= 'table' or type(t2) ~= 'table' then return false end
+  
+  local diff = {}
+  
+  -- Simple diff here, speed is Log(n)  
+  for i, v in pairs(t1) do
+    if t1[i] ~= t2[i] then
+      diff.insert(t1[i])
+    end    
+  end
+  return diff
+end
+
+--  [[  Updates multiple items in an API ]]  --
 --  [[  !Converts data to JSON ]]  --
 --  [[  !Uses LUA table for request_body  ]]  --
---  [[  !Tables can only be 1 level deep  ]]  --
-function UpdateItemsInAPI(APISettings, dbData)  
-  if(type(dbData) ~= 'table'      then return end
+function UpdateItemsInAPI(APISettings, data)  
+  if(type(data) ~= 'table'      then return end
   if(APISettings.ip == nil)       then return end
   if(APISettings.port == nil)     then return end
   if(APISettings.endpoint == nil) then return end
@@ -41,8 +56,8 @@ function UpdateItemsInAPI(APISettings, dbData)
 
 --  [[  Go through database entries updating them 1 by 1  ]]  --
 --  [[  TODO: Multiple Items at once(SUPPORT FROM API NEEDED)  ]]  --
-  for i,v in pairs(dbData) do
-      local APIUrl = APISettings.ip .. ':' .. APISettings.port .. '/' .. APISettings.endpoint .. '/' .. v.id
+  for i,v in pairs(data) do
+      local APIUrl = string.format('%s:%s/%s/%d', APISettings.ip, APISettings.port,APISettings.endpoint,v.id)
       local request_body, response_body = {}
       
       request_body = {v.name, v.address, v.comment}
@@ -61,12 +76,14 @@ function UpdateItemsInAPI(APISettings, dbData)
       }
       
       if (code ~= 200) then
-        log('device_change_check:' .. 
+        -- TODO: Test this stuff
+        log(string.format('ERROR IN FILE: device_change_check: %s : API returned code %d with the following response: %s', __LINE__(), code, res))
       end
-      
-  end  
+  end
+  return true
 end
 
+--  [[  __Implementation__  ]]  --
 local APISETTINGS = {
     ip = '0.0.0.0',
     port = '5000',
@@ -80,13 +97,17 @@ local STORAGEDETAILS = {
 APISETTINGS = CreateConstant(APISettings)
 STORAGEDETAILS = CreateConstant(STORAGEDETAILS)
 
---  [[  __Implementation__  ]]  --
 currentDevices = db:getall('SELECT id, name, address, comment FROM objects')
 oldDevices = storage.get(STORAGEDETAILS.string)
 if (oldDevices == nil) then 
   storage.set(STORAGEDETAILS.string)
 else
   if(currentDevices ~= oldDevices) then
-    
+    local diff = GetTableDiff(currentDevices, oldDevices)
+    if UpdateItemsInAPI(APISETTINGS, diff) == true then
+      storage.set(STORAGEDETAILS.string, currentDevices)
+    else
+      UpdateItemsInAPI(APISETTINGS, oldDevices) -- Return to old state
+    end
   end
 end
